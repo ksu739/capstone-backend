fc11719b8b738a4d2855a84524b11b3e5d30c2cc
ksu739 <ksu739@naver.com>
2021-09-25 01:40:45 +0900

    heroku test

---
+++ config/db_connect.js
@@ -0,0 +1,8 @@
+const setup = {
+    host: "nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: "3306",
+    user: "tnqi51giv5ki1tvk",
+    password: "f2qj8atatqlibl66",
+    database: "qffzf38aeo56prj5",
+}
+module.exports = setup
--- index.js
+++ index.js
@@ -1,46 +1,92 @@
-const e = require('express')
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+// add dependencies
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const morgan = require("morgan")
+const mysql = require("mysql");
+const path = require("path");
 
-user_list = [
-    {
-        id:123,
-        pw:123,
-        name:"aa"
-    },
-    {
-        id:456,
-        pw:456,
-        name:"bbc"
-    }
-]
+// add config
+const setup = require('./config/db_connect');
+const port = process.env.PORT || 3000;
 
+// middleware
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(morgan("dev"))
 
 
-app.get("/user", (req,res) => {
-    id = req.query.id
-    pwd = req.query.pwd
+const pool = mysql.createPool(setup);
 
-    function isuser(el1) {
-        if (el1.id == id && el1.pw == pwd) {
-            return true
+
+app.get("/", (req, res) => {
+    res.send("api페이지 입니다");
+});
+
+app.post("/login", (req, res) => {
+    // /login POST BODY(JSON(id:1,password:1))
+    const id = req.body["id"];
+    const password = req.body["pwd"];
+
+    pool.getConnection(function (err, conn) {
+        if (!err) {
+            conn.query(
+                `select * from test where id = ? and pwd = ?`,
+                [id, password],
+                function (err, result) {
+                    if (err) res.status(400).send(err);
+                    else if (result.length == 0)
+                        res.status(401).json({ Error: "No_Data" });
+                    else {
+                        const user = Object.values(JSON.parse(JSON.stringify(result)))[0];
+                        res.status(200).json(user);
+                    }
+
+                    conn.release();
+                }
+            );
+        } else {
+            res.json({ mysqlError: err });
         }
-    }
+    });
+});
 
-    const user = user_list.find(isuser)
-    if (user == null) {
-        res.status(400).json({"error":"Not User"})
-    }
-    else
-        res.status(200).json({user})
+app.post("/signup", (req, res) => {
+    const id = req.body["id"];
+    const pwd = req.body["pwd"];
+    const phone = req.body["phone"];
 
-})
+    pool.getConnection(function (err, conn) {
+        if (!err) {
+            conn.query(
+                `insert into TEST VALUES(?,?,?)`,
+                [id, pwd, phone],
+                function (err, result) {
+                    try {
+                        if (result != null)
+                            res.status(201).json({
+                                "success": "done"
+                            })
+                        else if (result == null)
+                            res.status(400).json({
+                                "success": "fail",
+                                "message": "same id exist"
+                            })
 
-app.get('/', (req, res) =>  {
-    res.send("ASDASD")
-    }
-)
+                    } catch (error) {
+                        console.log(error)
+                        res.status(400)
+                    }
+                }
 
+            );
+            conn.release();
 
-app.listen(port, () => console.log("Example app listening on port port!"))
+        }
+        else {
+            res.status(400).send(err)
+        }
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port : ${port}!`));
