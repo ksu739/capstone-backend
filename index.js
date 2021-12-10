3ea2d72f58e3fa4ba5a1657117e7038bf398d49d
ksu739 <ksu739@naver.com>
2021-12-10 17:25:57 +0900

    update

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const bodyparser = require("body-parser");
 const morgan = require("morgan")
 const mysql = require("mysql");
 const path = require("path");
+const jwt = require("jsonwebtoken")
 
 // add config
 const setup = require('./config/db_connect');
+const { encode } = require("punycode");
 const port = process.env.PORT || 3000;
 
 // middleware
@@ -24,22 +26,35 @@ app.get("/", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-    // /login POST BODY(JSON(id:1,password:1))
-    const id = req.body["id"];
-    const password = req.body["pwd"];
+    // /login POST BODY(JSON(id:1,password:1)) 
+    
+    const id = req.body["id"]; // 학번   
+    const pwd = req.body["pwd"];
+    
 
     pool.getConnection(function (err, conn) {
         if (!err) {
             conn.query(
-                `select * from test where id = ? and pwd = ?`,
-                [id, password],
+                `select * from student where id = ? and pwd = ?`,
+                [id, pwd],
                 function (err, result) {
                     if (err) res.status(400).send(err);
                     else if (result.length == 0)
                         res.status(401).json({ Error: "No_Data" });
                     else {
                         const user = Object.values(JSON.parse(JSON.stringify(result)))[0];
-                        res.status(200).json(user);
+                        const token = jwt.sign({ id: id },
+                            "secret-key",
+                            {
+                                algorithm: "HS256",
+                                expiresIn: "1h",
+                            }
+                        )
+                        res.status(200).json({
+                            user: user,
+                            "token": token
+                        });
+
                     }
 
                     conn.release();
@@ -52,15 +67,20 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/signup", (req, res) => {
+    //이름 전공 학번(id) 전화번호 주소 주민번호 비밀번호
+    const name = req.body["name"];
+    const major = req.body["major"];
     const id = req.body["id"];
+    const phone =req.body["phone"];
+    const address = req.body["address"];
+    const secretnum = req.body["secretnum"];
     const pwd = req.body["pwd"];
-    const phone = req.body["phone"];
-
+    
     pool.getConnection(function (err, conn) {
         if (!err) {
             conn.query(
-                `insert into TEST VALUES(?,?,?)`,
-                [id, pwd, phone],
+                `insert into student VALUES(?,?,?,?,?,?,?)`,
+                [name, major, id,phone,address,secretnum,pwd],
                 function (err, result) {
                     try {
                         if (result != null)
@@ -89,4 +109,47 @@ app.post("/signup", (req, res) => {
     });
 });
 
+app.post("/test", (req, res) => {
+    const JWT = req.headers.jwt
+    console.log(JWT)
+    try {
+        jwt.verify(JWT, "secret-key")
+        res.send({ ok: true })
+    } catch {
+        res.send({ ok: false })
+    }
+})
+
+app.post("/check", (req, res) => {
+
+    const nodenum = req.body["nodenum"];
+    pool.getConnection(function (err, conn) {
+        if (!err) {
+            conn.query(
+                `select * from node where nodenum = ? `,
+                //select 유니크아이디, 자기해쉬 from 테이블 where 자기해쉬 = (select 윗놈해쉬 from 테이블 where  유니크아이디 + 1) 
+                [nodenum],
+                function (err, result) {
+                    if (err) res.status(400).send(err);
+                    else if (result.length == 0)
+                        res.status(401).json({ Error: "No_Data" });
+                    else {
+                        const user = Object.values(JSON.parse(JSON.stringify(result)))[0];
+                        
+                        res.status(200).json({
+                            
+                        });
+
+                    }
+
+                    conn.release();
+                }
+            );
+        } else {
+            res.json({ mysqlError: err });
+        }
+    });
+
+
+})
 app.listen(port, () => console.log(`Example app listening on port : ${port}!`));
